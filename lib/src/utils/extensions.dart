6bc8d1eccf2a45fb67a514f95188bc4cb20c1b54
franzap <_@franzap.com>
2025-07-07 09:53:35 -0300

    String encodeShareable()

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -7,8 +7,8 @@ extension StringMaybeExt on String? {
 }
 
 extension StringExt on String {
-  String? encodeShareable() => Utils.encodeShareable(this);
-  String? decodeShareable() => Utils.decodeShareable(this);
+  String encodeShareable() => Utils.encodeShareable(this);
+  String decodeShareable() => Utils.decodeShareable(this);
 }
 
 extension DateTimeExt on DateTime {
